8082f3a9f8d994f7f76ffc4d532e957a38546837
wangding0421 <wangding0421@gmail.com>
2016-05-26 15:34:08 -0700

    Added problem 172

---
+++ Leetcode/172-FactorialTrailingZeroes/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+
+  int trailingZeroes(int n) {
+    int result = 0;
+    for (long long i = 5; n/i > 0; i *= 5){
+      result += n/i;
+    }
+    return result;
+  }
+};
+
+int main()
+{
+  Solution s;
+  cout << "Result of 4616 is: " << s.trailingZeroes(4616) << endl;
+  return 0;
+}
+
+
